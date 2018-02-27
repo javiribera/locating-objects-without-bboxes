update to pythorch 0.3 and fix 1x1 normalization layer
Former-commit-id: 402604b46ef9d13bfcee81444859835d7e14e7b3

plant-locator/models/unet_model.py
@@ -29,7 +29,7 @@ class UNet(nn.Module):
         self.down5 = down(512, 512)
         self.down6 = down(512, 512)
         self.down7 = down(512, 512)
-        self.down8 = down(512, 512)
+        self.down8 = down(512, 512, normaliz=False)
         self.up1 = up(1024, 512)
         self.up2 = up(1024, 512)
         self.up3 = up(1024, 512)

plant-locator/models/unet_parts.py
@@ -7,19 +7,22 @@ import torch.nn.functional as F
 
 
 class double_conv(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_ch, out_ch, normaliz=True):
         super(double_conv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            # nn.Dropout(p=0.1),
-            # TODO: BATCH NORM WITH A BATCH SIZE OF 1 crashes w/ pytorch 0.3
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            # nn.Dropout(p=0.1),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
-        )
+
+        ops = []
+        ops += [nn.Conv2d(in_ch, out_ch, 3, padding=1)]
+        # ops += [nn.Dropout(p=0.1)]
+        if normaliz:
+            ops += [nn.BatchNorm2d(out_ch)]
+        ops += [nn.ReLU(inplace=True)]
+        ops += [nn.Conv2d(out_ch, out_ch, 3, padding=1)]
+        # ops += [nn.Dropout(p=0.1)]
+        if normaliz:
+            ops += [nn.BatchNorm2d(out_ch)]
+        ops += [nn.ReLU(inplace=True)]
+
+        self.conv = nn.Sequential(*ops)
 
     def forward(self, x):
         x = self.conv(x)
@@ -37,11 +40,11 @@ class inconv(nn.Module):
 
 
 class down(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_ch, out_ch, normaliz=True):
         super(down, self).__init__()
         self.mpconv = nn.Sequential(
             nn.MaxPool2d(2),
-            double_conv(in_ch, out_ch)
+            double_conv(in_ch, out_ch, normaliz=normaliz)
         )
 
     def forward(self, x):
6a1253ab04251d118e66626c2b7d5cd96a89e7a9
Javi Ribera <jprat@purdue.edu>
2018-05-09 01:19:09 -0400

    v1.2.0
    Former-commit-id: 0f7cb002914d42933d08efcb381e247012532999

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='object-locator',
-    version='1.1.0',
+    version='1.2.0',
     description='Object Location using PyTorch (inference/testing only).',
 
     # The project's main homepage.

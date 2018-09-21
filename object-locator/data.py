605d9d3d317e866acccb6680dae4c18f4eadfae6
Javi Ribera <jprat@purdue.edu>
2018-09-21 16:03:09 -0400

    added copyright notices to all files
    Former-commit-id: 288288b604e9002cceabd6fafd69459ab6d5719a

--- object-locator/data.py
+++ object-locator/data.py
@@ -1,3 +1,11 @@
+# Copyright &copyright 2018 The Board of Trustees of Purdue University.
+# All rights reserved.
+# 
+# This source code is not to be distributed or modified
+# without the written permission of Edward J. Delp at Purdue University
+# Contact information: ace@ecn.purdue.edu
+# =====================================================================
+
 import os
 import random
 from collections import OrderedDict

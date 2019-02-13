a6936a47bda57ff17b86d3a3559654f0c63eb46e
Javi Ribera <jprat@purdue.edu>
2019-02-12 19:50:08 -0800

    update version number in python package
    Former-commit-id: d6ae6f71c54293cc8e91f7bcb02818d0433d6df4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='object-locator',
-    version='1.3.1',
+    version='1.5.0',
     description='Object Location using PyTorch.',
 
     # The project's main homepage.
